9540877151005699ec5c1b4b2d81673a704b6d4e
Soren Martius <soeren.martius@gmail.com>
2019-10-10 15:53:58 +0200

    add rust kata Product of consecutive Fib numbers

--- rust/7-kyu/even-numbers-in-array/src/lib.rs
+++ rust/7-kyu/even-numbers-in-array/src/lib.rs
@@ -1,4 +1,4 @@
-/// # Even Numbers in Arraz
+/// # Even Numbers in Array
 ///
 /// Given an array of digital numbers, return a new array of length number containing the last even numbers from the original array (in the same order).
 /// The original array will be not empty and will contain at least "number" even numbers.
